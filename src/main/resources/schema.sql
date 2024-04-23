8e547f9ba653d03752252d2a0819c989bfc5f75f
Luan de Oliveira <luan012012@hotmail.com>
2024-04-22 22:41:48 -0300

    feat: alterado schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,17 @@
-CREATE TABLE IF NOT EXISTS Users(
-    username VARCHAR(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS USERS(
+    username VARCHAR(50) NOT NULL primary key,
     password VARCHAR(50) NOT NULL,
     enabled BOOLEAN NOT NULL
 );
 
 INSERT INTO USERS(username, password, enabled) VALUES('loliveirap', 'c2XpY3VyaW9zb2Vt', true);
+
+CREATE TABLE IF NOT EXISTS AUTHORITIES(
+   username VARCHAR(50) NOT NULL,
+   authority VARCHAR(50) NOT NULL,
+   CONSTRAINT FK_AUTHORITIES_USERS FOREIGN KEY (username) REFERENCES USERS(username),
+   CONSTRAINT USERNAME_AUTHORITY UNIQUE(username, authority)
+);
+
+INSERT INTO AUTHORITIES(username, authority) VALUES('loliveirap', 'ROLE_ADMIN');
+INSERT INTO AUTHORITIES(username, authority) VALUES('loliveirap', 'ROLE_USER');
